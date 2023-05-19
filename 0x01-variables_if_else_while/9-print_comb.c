9c41f6dc5b97dbaf2333525f099a38fadb4fbec1
ingrid <ingrid.mhlaba@icloud.com>
2023-05-19 02:58:16 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= \58; num++)
+	{
+		putchar((num)
+		if (num != 58)
+		{
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
