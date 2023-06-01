5a1cd47e2577de7e827a356c3b740a4d853d9b0b
ingrid <ingrid.mhlaba@icloud.com>
2023-06-01 04:17:07 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	long int n;
+	long int max;
+	long int i;
+
+	n-612852475143;
+	max = -1;
+
+	while (n % 2 ==0)
+	{
+		max=2;
+		n/=2;
+        }
+}
+if (n > 2)
+	max =n
+printf('%ld\n",nax);
+return(0)
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On erro, -1 is returned, and errno is set appropriately
+ */
+int_putcher(char c)
+{
+	return (wriite(1,&c, 1));
+}
